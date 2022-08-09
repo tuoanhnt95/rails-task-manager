fecbe4271904a75ce6fc7db9c28dd32ad5490c00
Oanh Nguyen <myspamvn@yahoo.com>
2022-08-09 14:08:41 +0900

    show or hide completed in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create', as: :tasks
   get '/tasks', to: 'tasks#index'
